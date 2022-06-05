6a871a9fc0f7dfe35d2509f5b382a88a450e1a74
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-05 18:23:10 +0400

    commit-011 - calculated hash )

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,12 @@
+import fs from "fs";
+import crypto from "crypto";
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const temp = await fs.promises.readFile('src/hash/files/fileToCalculateHashFor.txt');
+
+    const hashSum = crypto.createHash("sha256").update(temp).digest("hex");
+
+    console.log(hashSum);
 };
+
+calculateHash();
