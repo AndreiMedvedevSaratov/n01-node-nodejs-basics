a3147291b275e931019f10cff88ad7bde46ee160
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-05 18:50:12 +0400

    commit-013 - created decompression!

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,15 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const decompress = async () => {
-    // Write your code here 
+    const stream = createReadStream('src/zip/files/archive.gz');
+
+    stream
+        .pipe(createUnzip())
+        .pipe(createWriteStream('src/zip/files/fileToCompress.txt'))
+        .on('finish', () => {
+            console.log('Decompression completed!')
+        })
 };
+
+decompress();
