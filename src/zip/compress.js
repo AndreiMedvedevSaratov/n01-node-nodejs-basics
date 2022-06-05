16303313ece8d449ade5758921883f0daac141e2
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-05 18:30:59 +0400

    commit-012 - created compression!

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,15 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const stream = createReadStream('src/zip/files/fileToCompress.txt');
+
+    stream
+        .pipe(createGzip())
+        .pipe(createWriteStream('src/zip/files/archive.gz'))
+        .on('finish', () => {
+            console.log('Compression completed!');
+        })
 };
+
+compress();
