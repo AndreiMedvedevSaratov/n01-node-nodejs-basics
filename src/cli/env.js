0923521a466945a5c9d192b69093ad35fe2c1894
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-05 21:17:43 +0400

    commit-015 - created cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,19 @@
 export const parseEnv = () => {
-    // Write your code here 
+    process.env.RSS_name1 = 'value1';
+    process.env.RSS_name2 = 'value2';
+    process.env.RSS_name3 = 'value3';
+    process.env.RSS_name4 = 'value4';
+    process.env.RSS_name5 = 'value5';
+
+    let result = '';
+
+    for (const [key, value] of Object.entries(process.env)) {
+        if (key.includes('RSS_')) {
+            result += `${key}: ${value}; `;
+        }
+    }
+
+    console.log(result);
 };
+
+parseEnv();
