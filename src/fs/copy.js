0cd480639fe9b3998951037261f4172939296498
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-05 17:59:27 +0400

    commit-005 - fs/copy.js created ))

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,18 @@
+import * as fs from 'fs';
+
 export const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.mkdir('src/fs/files_copy');
+        await fs.promises.access('src/fs/files/');
+    } catch {
+        throw new Error("FS operation failed");
+    }
+
+    fs.cp('src/fs/files/', 'src/fs/files_copy/', { recursive: true }, (error) => {
+        if (error) {
+            throw new Error("FS operation failed");
+        }
+    });
 };
+
+copy();
