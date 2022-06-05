a441714651a4dd785d0b1768aa089d487c3170f6
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-05 15:41:32 +0400

    commit-004 - fs/rename.js created ))

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,16 @@
+import * as fs from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+    try {
+        if (fs.existsSync('src/fs/files/properFilename.md') || !fs.existsSync('src/fs/files/wrongFilename.txt')) {
+            throw console.log(new Error('FS operation failed'));
+        } else {
+            fs.rename('src/fs/files/wrongFilename.txt', 'src/fs/files/properFilename.md', () => {
+                console.log('File is renamed successfully.');
+            });
+        }
+    } catch (err) { }
+
 };
+
+rename();
