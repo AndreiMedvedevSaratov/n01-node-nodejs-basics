430bb87ba98b4ff795fd530188f0b4c7a038f0be
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-05 18:05:05 +0400

    commit-008 - streams/read.js created ))

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,20 @@
+import * as fs from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+
+    const readStream = fs.createReadStream('src/streams/files/fileToRead.txt', { encoding: "utf-8" });
+
+    readStream
+        .on("data", (chunk) => {
+            process.stdout.write(chunk);
+        })
+        .on("error", (error) => {
+            process.stdout.write(error);
+        })
+        .on("end", () => {
+            readStream.destroy();
+        });
+
 };
+
+read(); 
